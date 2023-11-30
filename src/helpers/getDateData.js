6fc645718b52f10bb1c76c31e116ed1d91e1c0f1
Nazar Tymiv <nazartumiv@gmail.com>
2023-11-30 23:43:14 +0000

    Fixed problems with backgrounds and themes :)

--- src/helpers/getDateData.js
+++ src/helpers/getDateData.js
@@ -13,15 +13,15 @@ export const getCurrentSeason = (setCurrentSeason) => {
 };
 
 export const getCurrentTime = (setCurrentTime, sunData) => {
-    const currentHour = new Date().getHours();
-    const sunrise = new Date(`July 21, 1983 ${sunData.sunrise}`).getHours();
-    const sunset = new Date(`July 21, 1983 ${sunData.sunset}`).getHours();
+    const currentHour = new Date().getUTCHours();
+    const sunrise = new Date(`July 21, 1983 ${sunData.sunrise}`).getUTCHours();
+    const sunset = new Date(`July 21, 1983 ${sunData.sunset}`).getUTCHours();
 
     if (currentHour >= sunrise && currentHour < 12) {
         setCurrentTime("morning");
     } else if (currentHour >= 12 && currentHour < sunset) {
         setCurrentTime("afternoon");
-    } else if (currentHour >= sunset && currentHour < sunrise) {
+    } else if (currentHour >= sunset) {
         setCurrentTime("evening");
     }
 };
